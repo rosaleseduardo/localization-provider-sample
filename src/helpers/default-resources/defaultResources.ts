57c0280a3b0a68571d8591f174d994fda6ada98a
Eduardo Rosales <eduardorosales.dev@gmail.com>
2025-04-02 07:05:29 -0500

    feat: eeee

--- src/helpers/default-resources/defaultResources.ts
+++ src/helpers/default-resources/defaultResources.ts
@@ -1,20 +1,20 @@
-import type { Resource } from 'i18next';
+import type { ResourceLanguage } from 'i18next';
 
 import type { Locale, SupportedLanguages } from '@interfaces';
 import * as supportedLanguages from '@locales';
 
 import { availableLocales } from '../available-locales';
 
-const defaultResources = (languges: SupportedLanguages = supportedLanguages) =>
+const defaultResources = (languages: SupportedLanguages = supportedLanguages) =>
   availableLocales.reduce(
     (acc, current) => {
-      const { i18n } = languges[current];
+      const { i18n } = languages[current];
 
       acc[current] = { ...i18n.namespaces };
 
       return acc;
     },
-    {} as Record<Locale, Resource>,
+    {} as Record<Locale, ResourceLanguage>,
   );
 
 export default defaultResources;
--- src/interfaces/LocalizationProvider.ts
+++ src/interfaces/LocalizationProvider.ts
@@ -1,8 +1,6 @@
 import type { Resource } from 'i18next';
 
-import type { Locale } from '@interfaces';
-
 export interface LocalizationProviderProps {
   children: React.ReactElement;
-  resources?: Record<Locale, Resource>;
+  resources?: Resource;
 }
